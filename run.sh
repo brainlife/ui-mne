8b89893a1c5a2b0d285aef57e702fc83ee729c11
Soichi Hayashi <soichih@gmail.com>
2021-04-06 20:41:45 -0400

    no longer creating output.ipynb

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ set -x
 echo "trusting main.ipynb"
 jupyter trust /main.ipynb
 
-echo "running notebook prior to opening it"
-jupyter nbconvert --to notebook --execute /main.ipynb --output /output.ipynb
-jupyter trust /output.ipynb
+#running before opening just updates static output. user still has to rerun cell
+#so that data will be loaded into memory, and plots to become interactable
+#echo "running notebook prior to opening it"
+#jupyter nbconvert --to notebook --execute /main.ipynb --output /output.ipynb
+#jupyter trust /output.ipynb
 
 echo "starting jupyter notebook server"
 
@@ -16,7 +18,6 @@ cat <<EOF > /config.py
 c.NotebookApp.allow_root = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 80
-#c.NotebookApp.token = '$TOKEN'
 c.NotebookApp.token = ''
 c.NotebookApp.base_url = '$BASEURL'
 #c.NotebookApp.webapp_settings = {'static_url_prefix':'$BASEURL'}
@@ -25,7 +26,7 @@ EOF
 echo "debug.."
 cat /config.py
 
-echo "starting notebook"
-jupyter notebook --config=/config.py ./output.ipynb
+echo "starting jupyter server"
+jupyter notebook --config=/config.py #./output.ipynb
 
 
